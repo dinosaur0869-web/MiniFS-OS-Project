ba1a675d99fb1ae8822eb9980df53bd7c0239b64
dinosaur0869-web <dinosaur0869@gmail.com>
2025-12-15 16:40:54 +0800

    Update path.c

--- path.c
+++ path.c
@@ -1,1 +1,24 @@
+#include <string.h>
+#include "path.h"
 
+int split_path(const char *path, char parts[][32]) {
+    int count = 0;
+    char temp[256];
+
+    if (path == NULL || strlen(path) == 0)
+        return 0;
+
+    // 複製一份，避免破壞原字串
+    strcpy(temp, path);
+
+    // 如果是絕對路徑，跳過前面的 '/'
+    char *token = strtok(temp, "/");
+
+    while (token != NULL) {
+        strcpy(parts[count], token);
+        count++;
+        token = strtok(NULL, "/");
+    }
+
+    return count;
+}
