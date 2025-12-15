0ab62576e8fd02d84ae8c9cb928103f821f05aec
dinosaur0869-web <dinosaur0869@gmail.com>
2025-12-15 16:36:06 +0800

    Update command.c

--- command.c
+++ command.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include "command.h"
 
+void execute_command(char *input) {
+    char cmd[32], arg[128];
+
+    arg[0] = '\0';
+
+    sscanf(input, "%s %127[^\n]", cmd, arg);
+
+    if (strcmp(cmd, "ls") == 0) {
+        cmd_ls(arg);
+    } 
+    else if (strcmp(cmd, "mkdir") == 0) {
+        cmd_mkdir(arg);
+    } 
+    else if (strcmp(cmd, "touch") == 0) {
+        cmd_touch(arg);
+    } 
+    else {
+        printf("Unknown command: %s\n", cmd);
+    }
+}
+
+void cmd_ls(char *path) {
+    if (strlen(path) == 0)
+        printf("[ls] current directory\n");
+    else
+        printf("[ls] path = %s\n", path);
+}
+
+void cmd_mkdir(char *path) {
+    printf("[mkdir] path = %s\n", path);
+}
+
+void cmd_touch(char *path) {
+    printf("[touch] path = %s\n", path);
+}
