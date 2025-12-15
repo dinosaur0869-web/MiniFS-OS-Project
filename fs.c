868e1d646f19ef782f04b51fc006bd7d2f2df3bb
dinosaur0869-web <dinosaur0869@gmail.com>
2025-12-16 01:45:25 +0800

    Update fs.c

--- fs.c
+++ fs.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fs.h"
+
 FSNode* root;
 FSNode* cwd; // current working directory
 
 void fs_init() {
-    root = malloc(sizeof(FSNode));
+    root = (FSNode*)malloc(sizeof(FSNode));
     strcpy(root->name, "/");
     root->type = TYPE_DIR;
     root->parent = NULL;
     root->child_count = 0;
 
     cwd = root;
+
+    printf("[fs] initialized\n");
 }
